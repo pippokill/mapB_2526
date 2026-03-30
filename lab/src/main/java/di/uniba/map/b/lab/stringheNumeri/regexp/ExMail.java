3913c17dabaa3303b300c48026af9619b964933d
pippokill <pierpaolo.basile@gmail.com>
2026-03-30 15:25:54 +0200

    Collections

--- lab/src/main/java/di/uniba/map/b/lab/stringheNumeri/regexp/ExMail.java
+++ lab/src/main/java/di/uniba/map/b/lab/stringheNumeri/regexp/ExMail.java
@@ -5,25 +5,37 @@
  */
 package di.uniba.map.b.lab.stringheNumeri.regexp;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  *
  * @author pierpaolo
  */
 public class ExMail {
-    
+
     /**
      *
      */
-    public static String EXP_MAIL="[a-zA-Z][\\w]*[\\.]?[\\w]+@[a-zA-Z]+\\.[a-zA-Z]{2,3}";
+    public static final String EXP_MAIL = "([a-zA-Z][\\w]*[\\.]?[\\w]+)@([a-zA-Z]+)(\\.[a-zA-Z]{2,3})";
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        if (args.length>0) {
+        if (args.length > 0) {
             System.out.println(EXP_MAIL);
             System.out.println(args[0].matches(EXP_MAIL));
+            System.out.println("===GROUPS===");
+            Pattern p = Pattern.compile(EXP_MAIL);
+            Matcher m = p.matcher(args[0]);
+            while (m.find()) {
+                int g = m.groupCount();
+                for (int i = 0; i <= g; i++) {
+                    System.out.println(" Group: " + m.group(i) + " start: " + m.start(i) + " " + m.end(i));
+                }
+            }
         }
     }
-    
+
 }
